5ee1241ca1a1c3f9c306e30da2bcada4d18061e1
Wilhelm Zimmermann <wilhelmzimmermann8@gmail.com>
2025-08-12 18:44:55 -0300

    feat: added deletion and get paginated tests

---
+++ tests/HomeControllerHUB.Application.Tests/Establishments/Commands/DeleteEstablishmentCommandTest.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using HomeControllerHUB.Application.Establishments.Commands.CreateEstablishment;
+using HomeControllerHUB.Domain.Interfaces;
+using HomeControllerHUB.Globalization;
+using Moq;
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using HomeControllerHUB.Application.Establishments.Commands.DeleteEstablishment;
+using HomeControllerHUB.Application.Tests.Utils;
+using HomeControllerHUB.Domain.Models;
+
+namespace HomeControllerHUB.Application.Tests.Establishments.Commands;
+
+public class DeleteEstablishmentCommandTest : TestConfigs
+{
+    private readonly Mock<IMapper> _mapperMock;
+    private readonly Mock<ISharedResource> _resourceMock;
+    private readonly Mock<ICurrentUserService> _currentUserServiceMock;
+    
+    public DeleteEstablishmentCommandTest()
+    {
+        _mapperMock = new Mock<IMapper>();
+        _resourceMock = new Mock<ISharedResource>();
+        _currentUserServiceMock = new Mock<ICurrentUserService>();
+    }
+
+    [Fact]
+    public async Task Delete_Should_Succeed_WithValidParameters()
+    {
+        // ARRANGE
+        var authUserId = Guid.NewGuid();
+        _currentUserServiceMock.Setup(s => s.UserId).Returns(authUserId);
+
+        var newEstablishment = await CreateEstablishment();
+
+        var user = UserHelpers.GetApplicationUser();
+        user.Id = authUserId;
+        user.EstablishmentId = newEstablishment.Id;
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        var command = new DeleteEstablishmentCommand(newEstablishment.Id);
+        
+        var handler = new DeleteEstablishmentCommandHandler(_context, _resourceMock.Object);
+
+        // ACT
+        await handler.Handle(command, CancellationToken.None);
+
+        // ASSERT
+        var establishmentInDb = await _context.Establishments.FindAsync(newEstablishment.Id);
+        
+        establishmentInDb.Enable.Should().BeFalse();
+    }
+    
+    [Fact]
+    public async Task Delete_Should_ThrowAppError_WithInvalidId()
+    {
+        // ARRANGE
+        var authUserId = Guid.NewGuid();
+        _currentUserServiceMock.Setup(s => s.UserId).Returns(authUserId);
+
+        var newEstablishment = await CreateEstablishment();
+
+        var user = UserHelpers.GetApplicationUser();
+        user.Id = authUserId;
+        user.EstablishmentId = newEstablishment.Id;
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        var command = new DeleteEstablishmentCommand(new Guid("cdecd467-9729-42d5-8694-9189f1a7a234"));
+        
+        var handler = new DeleteEstablishmentCommandHandler(_context, _resourceMock.Object);
+
+        // ACT
+        var act = async () => await handler.Handle(command, CancellationToken.None);
+
+        // ASSERT
+        act.Should()
+            .ThrowAsync<AppError>()
+            .Where(x => x.StatusCode == 404);
+    }
+}
---
+++ tests/HomeControllerHUB.Application.Tests/Establishments/Queries/GetAllEstablishmentPaginatedQueryTests.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using FluentAssertions;
+using HomeControllerHUB.Application.Establishments.Queries.GetAllEstablishmentPaginated;
+using HomeControllerHUB.Domain.Interfaces;
+using HomeControllerHUB.Domain.Mappings;
+using HomeControllerHUB.Globalization;
+using Moq;
+
+namespace HomeControllerHUB.Application.Tests.Establishments.Queries;
+
+public class GetAllEstablishmentPaginatedQueryTests : TestConfigs
+{
+    private readonly Mock<ISharedResource> _resourceMock;
+    private readonly IMapper _mapper;
+    private readonly Mock<ICurrentUserService> _currentUserServiceMock;
+
+    public GetAllEstablishmentPaginatedQueryTests()
+    {
+        _resourceMock = new Mock<ISharedResource>();
+        _currentUserServiceMock = new Mock<ICurrentUserService>();
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile(new MappingProfile(typeof(GetAllEstablishmentPaginatedQueryHandler).Assembly));
+        });
+        
+        _mapper = mapperConfig.CreateMapper();
+    }
+
+
+    [Fact]
+    public async Task Get_Should_Return_All_Establishments()
+    {
+        // ARRANGE
+        var newEstablishment = await CreateEstablishment();
+        
+        var query = new GetAllEstablishmentPaginatedQuery();
+        var handler = new GetAllEstablishmentPaginatedQueryHandler(_context, _currentUserServiceMock.Object, _mapper);
+        // ACT
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // ASSERT
+        result.Items.Should().HaveCount(1);
+        result.Items[0].Id.Should().Be(newEstablishment.Id);
+        result.Items[0].Name.Should().Be(newEstablishment.Name);
+    }
+}
